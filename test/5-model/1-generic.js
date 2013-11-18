7b05f3dbe439f5ba5765f49bf1f4a19d54b83177
Lorenzo García Rivera <lorenzogrv@gmail.com>
2013-11-18 18:55:14 +0100

    move model to lib/

--- test/5-model/1-generic.js
+++ test/5-model/1-generic.js
@@ -1,34 +1,35 @@
 var assert = require('chai').assert
   , iai = require('../../..')
   , test = iai('test')
+  , testOpts = { system: 'SQLite' };
 ;
 
 describe.only( 'generic model interface', function(){
   describe( 'Connection', function(){
     var Connection;
     it( 'should be required without errors', function(){
-      Connection = iai('plugins/model/core/Connection.js');
+      Connection = iai('plugins/model/Core/Connection.js');
     })
     it( 'should have the following api', function(){
-      test.methods( Connection(), 'open', 'close' );
+      test.methods( Connection(testOpts), 'open', 'close' );
     })
   })
   describe( 'DAO', function(){
     var DAO;
     it( 'should be required without errors', function(){
-      DAO = iai('plugins/model/core/DAO.js');
+      DAO = iai('plugins/model/Core/DAO.js');
     })
     it( 'should have the following api', function(){
-      test.methods( DAO(), 'create', 'retrieve', 'update', 'destroy' );
+      test.methods( DAO(testOpts), 'create', 'retrieve', 'update', 'destroy' );
     })
   })
   describe( 'Facade', function(){
     var Facade;
     it( 'should be required without errors', function(){
-      Facade = iai('plugins/model/core/Facade.js');
+      Facade = iai('plugins/model/Core/Facade.js');
     })
     it( 'should have the following api', function(){
-      test.methods( Facade(), 'find', 'findOne', 'create', 'update', 'destroy' );
+      test.methods( Facade(testOpts), 'find', 'findOne', 'create', 'update', 'destroy' );
     })
   })
 
