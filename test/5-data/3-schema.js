5f82ae6bb9bf019e034f992851653434cd564555
Lorenzo García Rivera <lorenzogrv@gmail.com>
2013-12-21 19:13:25 +0100

    save work on Data validation structures

--- test/5-data/3-schema.js
+++ test/5-data/3-schema.js
@@ -1,30 +1,27 @@
 var assert = require('chai').assert
   , iai = require('../..')
   , test = iai('test')
-  , Field = iai('data/Field')
+  , fields = iai('data/fields')
   , Schema = iai('data/Schema')
   , SchemaValidationError = iai('data/SchemaValidationError')
 ;
 
-describe.skip( 'Schema', function(){
+describe( 'Schema', function(){
   it( 'should be a builder', function(){
     test.builder( Schema, [] );
   })
   it( 'should have the following chainable api', function(){
     test.chainableApi( Schema(), {
-      'field': [ 'some_name', Field() ], // composite 'add'
-      'fields': [ {} ], // multiple composite 'add'
+      'addField': [ 'some_name', iai('data/Field')() ], // composite 'add'
+      'addFields': [ {} ], // multiple composite 'add'
     })
   })
   it( 'should validate as expected', function(done){
-    var schema = Schema()
-    .name( 'testing-schema' )
-    .description( 'this schema is meant for testing' )
-    .fields({
-      id: Field({ unique: true }),
-      name: Field.Text({ required: true, max_length: 30 }),
-      email: Field.Email({ required: true }),
-      password: Field.Text({ encrypt: "sha1" })
+    var schema = Schema( 'testing-schema', {
+      id: iai('data/Field')({ unique: true }),
+      name: fields('Text')({ max_length: 30 }),
+      email: fields('Text')({ }),
+      password: fields('Text')({ encrypt: "sha1", min_length: 6 })
     })
     ;
     schema.validate( {}, function(err, cleaned){
