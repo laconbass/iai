851d2ab964a55d2e22a82338ed9fc924521f5ab4
Lorenzo García Rivera <lorenzogrv@gmail.com>
2014-05-22 09:19:52 +0200

    cleanup, update iai-oop

--- test/5-data/3-schema.js
+++ test/5-data/3-schema.js
@@ -16,18 +16,34 @@ describe( 'Schema', function(){
       'addFields': [ {} ], // multiple composite 'add'
     })
   })
-  it( 'should validate as expected', function(done){
+  it.skip( 'should validate as expected', function(done){
     var schema = Schema( 'testing-schema', {
       id: iai('data/Field')({ unique: true }),
       name: fields('Text')({ max_length: 30 }),
-      email: fields('Text')({ }),
+      email: fields('Text')({  }),
       password: fields('Text')({ encrypt: "sha1", min_length: 6 })
-    })
-    ;
+    });
+
     schema.validate( {}, function(err, cleaned){
+      console.dir( err );
       assert.instanceOf( err, SchemaValidationError, "error should be present" );
       assert.isNull( cleaned, "cleaned data should be null when failed" );
-      console.dir( err.map )
+
+      assert.equal( err.code, "invalid_schema" )
+
+      assert.equal( err.errors.id.length, 1, "id error count not ok" );
+      assert.equal( err.errors.id[0].code, "empty", "id error code not ok" );
+
+      assert.equal( err.errors.name.length, 1, "name error count not ok" );
+      assert.equal( err.errors.name[0].code, "string", "name error code not ok" );
+
+      assert.equal( err.errors.email.length, 1, "email error count not ok" );
+      assert.equal( err.errors.email[0].code, "string", "email error code not ok" );
+
+      assert.equal( err.errors.password.length, 1, "password error count not ok" );
+      assert.equal( err.errors.password[0].code, "string", "password error code not ok" );
+
+      done();
     });
   })
 })
