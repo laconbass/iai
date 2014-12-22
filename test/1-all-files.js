c2400dd883151ea6c2480c0d981f9660ea2dad8c
Lorenzo García Rivera <lorenzogrv@gmail.com>
2014-12-22 17:43:34 +0100

    disable exports restrictions except empty exports

--- test/1-all-files.js
+++ test/1-all-files.js
@@ -9,7 +9,16 @@ var assert = require( "chai" ).assert
       "iai.load": iai.load
   }
   , should_export = {
-      "a function object": function( topic ) {
+      "not an empty object": function( topic ){
+        if(
+          'function' == typeof topic
+          // || 'string == typeof topic
+        ){
+          return "skip checking for empty object";
+        }
+        assert.isFalse( isEmptyObject(topic) );
+      }
+      /*,"a function object": function( topic ) {
           assert.isFunction( topic );
       }
       ,"a name string": function( topic ) {
@@ -21,9 +30,16 @@ var assert = require( "chai" ).assert
       ,"a stability number between 0 and 5": function( topic ) {
         assert.isNumber( topic.stability );
         assert.include( [0, 1, 2, 3, 4, 5], topic.stability )
-      }
+      }*/
   }
 ;
+
+function isEmptyObject( o ){
+  for( var n in o ){
+    return true;
+  }
+  return false;
+}
 
 var libRoot = join( __dirname, "..", "lib" )
   , file = require( "file" )
