5781a59ffca2ace514cf3f0f0329f594d3dd56eb
Lorenzo García Rivera <lorenzogrv@gmail.com>
2019-09-30 02:48:37 +0200

    bashido: small changes on api.argv automatic help routine

--- abc/bashido/abc/api/argv.bash
+++ abc/bashido/abc/api/argv.bash
@@ -4,10 +4,10 @@ function argv () {
   # runs the given command line according to specified mode
   # Usage: argv {MODE} [COMMAND]
   #
-  # argv will always assume its caller function name is part of executable name
+  # introspects $FUNCNAME assuming its caller name prefixes [COMMAND] name
   #
   # {MODE} may be one of: subcommand, command
-  ##
+  
   local funcname="${FUNCNAME[1]}"
   local executable
 
@@ -44,54 +44,69 @@ function argv () {
   then # executable is nothing, as has no type
     # it may be valid if requesting help
     case "$2" in help|--help|-h|-?)
+      # TODO research topic-specific help
+      if test $# -gt 2; then warn "help topic %s was provided" "$3"; fi
+
       read line funcname filename < <(caller 0)
       if ! read -d ':' begin < <(grep -n "function $funcname ()" "$filename"); then
         fail "can't find where $funcname is defined on $filename"
         return 1
       fi
       ((begin++)) # omit function declaration on help shown
-      if ! end=$(tail -n +$begin <"$filename" | grep -n -x "  ##"); then
-        warn "can't find help closing comment for $funcname at $filename"
+
+      if ! end=$(tail -n +$begin <"$filename" | grep -n -x "}"); then
+        warn "can't find closing curly brace for $funcname at $filename"
         end=$((line - begin))
       else
         read -d ':' end <<<"$end"
+        read -d ':' end < <( \
+          tail -n +$begin <"$filename" \
+          | head -n $end \
+          | grep -n -v -m 1 '^  #' \
+        ) && (( end-- )) || fail "$funcname has no help comments"
       fi
+     
+      # help title 
+      echo "# $funcname"; echo
 
-      # TODO research topic-specific help
-      if test $# -gt 2; then warn "help topic %s was provided" "$3"; fi
-      
       # dump the help comment from source file
-      echo "# $funcname #"
       tail -n +$begin "$filename" | head -n $end \
-        | while read l; do echo "${l##*( )}"; done
+        | while read l; do echo "${l##*( )}"; done \
+        | while read l; do echo "${l### }"; done
       echo
       
       # display usage info
+      echo '```'
       echo "usage: $funcname {$1} [$1 arguments]"
       echo "      '$funcname help' outputs this help text"
-      echo "  but '$funcname help [$1]' outputs specific help for [$1]"
+      #echo "  but '$funcname help [$1]' outputs specific help for [$1]"
+      echo '```'; echo
 
       # list available commands/subcommands
+      echo "## available ${1}s"; echo
       case "$1" in
         subcommand) prefix="$funcname." ;;
         command) prefix="$funcname-" ;;
       esac
-      printf "\navailable $1s:"
+      printf 'f.'
       while read d t fn; do echo $fn; done < <(declare -F) \
         | grep "$prefix" \
         | sort \
         | while read cmd; do printf " ${cmd##$prefix}"; done
+      printf \\n
+
       case "$1" in
         command)
-          while read -d ':' path; do
-            find "$path" -type f -name "$prefix*" -print0 \
+          printf 'F.'
+          while read path; do
+            test -d "$path" || continue
+            find "$path" -type f -name "$prefix*" -print0 2>/dev/null \
               | sort -z \
               | while IFS= read -d '' cmd; do printf " ${cmd##*/$prefix}"; done
-          done <<<"$PATH"
+            done < <( tr : \\n <<<"$PATH" )
           ;;
       esac
-      echo
-
+      printf \\n
       return 0
     esac
     # else it is not valid
@@ -100,7 +115,8 @@ function argv () {
     return 1
   fi
 
-  shift
+  shift # the MODE argument ($1)
+  shift # the argument being part of executable name ($2)
   verb "exec %s" "$executable $*"
   $executable "$@"
   # DON'T EXIT, let command decide when to exit (or return)
