c8ce73ab726ac2fe8259cdcd098a6580ba319c9c
Lorenzo Garcia Rivera <lorenzogrv@gmail.com>
2020-03-09 07:43:41 +0100

    add electron gui script for example. fix loading overlay animation

---
+++ packages/wui/example-backend.js
@@ -0,0 +1,19 @@
+
+const GUI = require('@iaigz/gui-electron')
+const os = require('os')
+
+let gui = new GUI()
+
+gui
+  .on('client:created', client => client.layout({
+    horizontal: 3, only: 1
+  }))
+  .on('request', (req, res) => {
+    console.warn('received request')
+    res.writeHead(301, { Location: 'https://127.0.0.1:9966' })
+    res.end()
+  })
+  .bootstrap()
+
+/* vim: set expandtab: */
+/* vim: set filetype=javascript ts=2 shiftwidth=2: */
