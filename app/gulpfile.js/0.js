a300aa2d4ac009de519c0056cd05f64bbbc394fb
Lorenzo García Rivera <lorenzogrv@mareagestion.com>
2020-03-10 20:32:30 +0100

    start research to fix the old gulpfile.js thing and extract reusable parts from there

--- app/gulpfile.js/0.js
+++ app/gulpfile.js/0.js
@@ -1,15 +1,15 @@
 // this gulpfile was splited into multiple files, each containing a task
 // following some guidelines found online.
 // see https://www.viget.com/articles/gulp-browserify-starter-faq
-const tasks = require('./lib/tasks')
-const gulp = tasks([
-  'browserify',
-  'watchify'
-])
+//var gulp = require('gulp')
 
-gulp.task('build', ['browserify'])
-gulp.task('watch', ['watchify'], require('./task/server.js'))
-gulp.task('default', ['watch'])
+//exports.build = require('./task/browserify.js')
+//exports.serve = require('./task/server.js')
+//exports.watchify = require('./task/watchify.js')
+
+//exports.watch = gulp.parallel(exports.watchify, exports.serve)
+
+//exports.default = exports.watch
 
 /* vim: set expandtab: */
 /* vim: set filetype=javascript ts=2 shiftwidth=2: */
